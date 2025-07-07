967dc609fe699ff12ff87b5187b7a66fb6784470
Riddick <danijel.galic@live.com>
2025-07-07 09:10:48 +0200

    Add the main landing file for HTTP requests (#7)
    Introduced the main app landing file (index.php) for handling all HTTP requests in the FireHub Web Application Framework. This is the starting point for all requests, ensuring they're handled efficiently and effectively.

---
+++ web/index.php
@@ -0,0 +1,19 @@
+<?php declare(strict_types = 1);
+
+/**
+ * This file is part of the FireHub Web Application Framework package
+ *
+ * This is the main app landing file for all HTTP requests.
+ * @since 1.0.0
+ *
+ * @author Danijel Galić <danijel.galic@outlook.com>
+ * @copyright 2025 FireHub Web Application Framework
+ * @license <https://opensource.org/licenses/OSL-3.0> OSL Open Source License version 3
+ *
+ * @package App\Web
+ *
+ * @version GIT: $Id$ Blob checksum.
+ */
+
+/** ### Register the Composer autoloader */
+require __DIR__.'/../vendor/autoload.php';
