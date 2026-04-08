4598d6c3fd4ca4b413fc91fdf6eb58c55529e488
Riddick <danijel.galic@live.com>
2026-04-08 10:10:24 +0200

    refactor: remove versioning comments and update namespace references

--- web/index.php
+++ web/index.php
@@ -12,12 +12,32 @@
  *
  * @php-version 8.5
  * @package App\Web
- *
- * @version GIT: $Id$ Blob checksum.
  */
-
-/** ### Register the Composer autoloader */
-require __DIR__.'/../vendor/autoload.php';
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+$start_time = microtime(true);
+// ----------------------------------------------------------------------------------
 
 /** ### Bootstrap FireHub Framework */
-echo (require __DIR__.'/../bootstrap/app.php')->boot();
+//echo (require __DIR__.'/../bootstrap/app.php')->boot();
+echo require '../../core-foundation/src/public/boot.php';
+//require 'phar://../vendor/firehub/core-foundation/dist/core.phar';
+
+/*use FireHub\Core\Support\DataStructure\ArrayCollection;
+
+$array = []; for ($i=0; $i < 1_000_000; $i++) $array[] = $i;
+
+$collection = new ArrayCollection($array);
+
+$collection->map(function ($value) {});*/
+
+// ----------------------------------------------------------------------------------
+if (PHP_SAPI === 'cli') {
+    echo "________________________________\n";
+    echo "Time spent: \e[1;37;42m".((microtime(true) - $start_time) * 1_000)."\e[0m\n";
+    echo "Peek: \e[1;37;41m".(number_format(memory_get_peak_usage()))."\e[0m\n";
+} else {
+    echo '<br><br>Time spent: <strong>'.(microtime(true) - $start_time) * 1_000 .'<br>';
+    echo '<br>Peek: <strong>'.number_format(memory_get_peak_usage()).' bytes</strong> of memory.<br>';
+}
